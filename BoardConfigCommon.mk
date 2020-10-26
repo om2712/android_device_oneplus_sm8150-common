4521e7430db398377aaa49e18e434594711267fd
mukesh22584 <mks22584@gmail.com>
2020-10-26 11:09:30 +0530

    sm8150-common: Let's move on Mady's kernel

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,6 +53,7 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_CONFIG := weeb_defconfig
 TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CONFIG := vendor/sm8150-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8150
 KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
