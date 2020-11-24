e0894e16fe74644b98cbf52293bfcf089718c248
mukesh22584 <mks22584@gmail.com>
2020-11-24 20:16:09 +0530

    sm8150-common: import Device Settings from A10 dt

--- DeviceSettings/Android.mk
+++ DeviceSettings/Android.mk
@@ -1,6 +1,7 @@
 #
 # Copyright (C) 2013 The CyanogenMod Project
 # Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2019 Android Open Source Illusion Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -33,6 +34,9 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
     $(TOP)/packages/resources/devicesettings/res
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    vendor.oneplus.camera.CameraHIDL-V1.0-java
 
 package_resource_overlays := $(strip \
     $(wildcard $(foreach dir, $(PRODUCT_PACKAGE_OVERLAYS), \
